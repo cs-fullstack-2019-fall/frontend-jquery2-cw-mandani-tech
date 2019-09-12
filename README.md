49099adb3863d3783bf937af812d77905299f6cf
mandani-tech <mandani.hamida@gmail.com>
2019-09-12 16:00:32 -0500

    cw with challenge

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # frontend-jQuery2-cw
 
-Use the provided HTML file to answer all of the provided JavaScript questions in the js.js file. The jQuery file is attached as well. Properly connect it to your HTML file.
+Use the provided HTML file to answer all of the provided JavaScript questions in the js.js file.
+ The jQuery file is attached as well. Properly connect it to your HTML file.
--- js.js
+++ js.js
@@ -3,10 +3,32 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 */
 
 
-/* Give everything with the class 'hello' a white background when you click on one of them*/
+/* Give everything with the class 'hello' a white yellow when you click on one of them*/
+
+
+$('.hello').css('backgroundColor','yellow');
 
 
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 
+
+$('h1').mouseenter(function ()
+{
+    console.log('event fired');
+    $('h1').css('height','50px','width','50px');
+
+
+});
+
+$('h1').mouseleave(function ()
+{
+    console.log('event fired');
+    $('h1').css('height','','width','');
+});
+
 // CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
+let elementp=$('p.hello  ');
+elementp.click(function (){
+    elementp.append('.');
+});
